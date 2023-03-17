30b56f7ff95d7b23a2c7cd73a4fdded624318f50
Moussa <ingmoussacly@hotmail>
2023-03-17 05:16:31 -0700

    puts func

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+/**
+* main - Entry point
+*
+* puts - print the string
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char msg[]="Programming is like building a multilingual puzzle";
+	char msg[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(msg);
-	return 0;
-}	
+	return (0);
+
+}
