465442d626b1b7d7efc80581eb0b5a5974d7ea37
Moussa <ingmoussacly@hotmail>
2023-03-17 05:24:19 -0700

    printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* printf - printing string in the ouput
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
