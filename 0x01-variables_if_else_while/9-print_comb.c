420f3700d07fedfaf956861d45223dcc88f9e47e
Moussa <ingmoussacly@hotmail>
2023-03-19 11:54:30 -0700

    single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,11 @@ int main(void)
 	for (ch = 48; ch <= 57; ch++)
 	{
 		putchar(ch);
-		putchar(44);
-		putchar(32);
+		if (ch != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
 	putchar(10);
 	return (0);
