071315ff7b6e9144e20282c889683d45b1849b3c
Moussa <ingmoussacly@hotmail>
2023-03-19 11:16:21 -0700

    reverse lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+	/*Looping through in reverse order*/
+	for (ch = 122; ch >= 97; ch--)
+	{
+		putchar(ch);
+	
+	}
+	putchar(10);
+	return (0);
+}
