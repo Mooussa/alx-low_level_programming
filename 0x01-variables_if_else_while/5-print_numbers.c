be561a7964448202f7f71abc8f7a6977c62fec46
Moussa <ingmoussacly@hotmail>
2023-03-19 08:31:48 -0700

    numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit;
+	/*Looping through the numbers*/
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+	return (0);
+}
