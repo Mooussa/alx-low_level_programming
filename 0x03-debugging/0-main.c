a75372b0a2691d9f5bf67a0c83a1f8b29655e699
Moussa <ingmoussacly@hotmail>
2023-03-23 17:01:46 -0700

    First bug fixes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 /**
 * main - tests function that prints if integer is positive or negative
 *
-* @i: integer
 * Return: 0
 */
 int main(void)
@@ -12,5 +11,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
+
 	return (0);
 }
