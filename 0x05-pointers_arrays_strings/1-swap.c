2360612f24be16a2d15036220052987119be36da
Moussa <ingmoussacly@hotmail>
2023-03-27 18:16:31 -0700

    Swap two numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* swap_int - Flip two numbers
+* @a: First number
+* @b: Second number
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 extern int _putchar(int c);
 extern void reset_to_98(int *n);
+extern void swap_int(int *a, int *b);
 
