d8ac245541dbfaace0ec4711dac3b2e9c7ea34d5
Moussa <ingmoussacly@hotmail>
2023-03-27 17:41:18 -0700

    Resetting value through pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - Reset the value of the pointer
+*
+* @n: The value holding a pointer
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+extern int _putchar(int c);
+extern void reset_to_98(int *n);
+
